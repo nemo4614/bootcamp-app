16fa17e0230d704d76376089655fd6458011f11c
nemo4614 <nemo4614@gmail.com>
2015-02-17 14:50:21 +0200

    Added heading, image, and link to other file

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
   <meta charset="UTF-8">
-  <title>Bootcamp</title>
+  <title>My form</title>
 </head>
 <body>
-  <h1>Hello world!</h1>
+  <h1>Hello World!</h1>
+  <form action="" method="post">
+    <label for="name">Name</label>
+    <input type="text" id="name" name="myname">
+    <label for"textfield">Text</label>
+    <input type="text" id="textfield" name="mytext">
+    <button type="submit">Send</button>
+  </form>
+  <a href="second.php">Link to other page</a>
+  <img src="http://www.amusingtime.com/images/021/may-be-blind-but-not-black-funny-meme.jpg" alt="funny meme">
 </body>
 </html>
