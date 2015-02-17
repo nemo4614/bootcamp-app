b83c2fbed6df9168283c00f23f67e35ff03407f6
nemo4614 <nemo4614@gmail.com>
2015-02-17 14:56:48 +0200

    Added unordered list and changed title

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>My form</title>
+  <title>Bootcamp</title>
 </head>
 <body>
   <h1>Hello World!</h1>
+  <ul>
+    <li>First link</li>
+    <li>Second link</li>
+    <li>Third link</li>
+  </ul>
   <form action="" method="post">
     <label for="name">Name</label>
     <input type="text" id="name" name="myname">
@@ -13,7 +18,10 @@
     <input type="text" id="textfield" name="mytext">
     <button type="submit">Send</button>
   </form>
+  <br>
   <a href="second.php">Link to other page</a>
+  <br>
+  <br>
   <img src="http://www.amusingtime.com/images/021/may-be-blind-but-not-black-funny-meme.jpg" alt="funny meme">
 </body>
 </html>
