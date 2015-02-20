72ec5e97c044345c758d6413b5db6edf93631760
nemo4614 <nemo4614@gmail.com>
2015-02-20 10:31:38 +0200

    fixed namespace bug in CircleTest.php

--- tests/CircleTest.php
+++ tests/CircleTest.php
@@ -6,7 +6,7 @@ class CircleTest extends PHPUnit_Framework_TestCase
 {
   public function testGetArea()
   {
-    $circle = new \Bootcamp\Demo\Circle(2);
+    $circle = new Circle(2);
     $this->assertEquals(12.566370614359172, $circle->getArea());
   }
 }
